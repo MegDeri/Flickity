86cfa58e1a3ca3f41bfc31b803ff2ae59c932f75
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-04 22:57:16 +0300

    Final changes in js & html & sass

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ var elem = document.querySelector('.main-carousel');
 /*var slideList = document.getElementById('carousel-cell').innerHTML;
 Mustache.parse(slideList);
 var slideCon = '';
+
 // Loop for creating slides
 for (var i = 0; i < slideData.length; i++) {
   console.log(slideData);
   slideCon += Mustache.render(slideList, slideData[i]);
-}*/
+}
 
-elem.insertAdjacentHTML('beforeend', slideCon);
+elem.insertAdjacentHTML('beforeend', slideCon);*/
 
 var flkty = new Flickity( elem, {
   // options
@@ -50,12 +51,7 @@ window.initMap = function() {
   });
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({
-  position: uluru,
-  map: map
+    position: uluru,
+    map: map
   });
 }
-
-
-
-
-
