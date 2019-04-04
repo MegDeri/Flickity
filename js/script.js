e7926ec319dcc15ee52013936c91e1ceeda24284
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-04 23:24:57 +0300

    Create mustache.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
 var elem = document.querySelector('.main-carousel');
+var slideList = document.getElementById('carousel-cell').innerHTML;
+Mustache.parse(slideList);
+var slideCon = '';
+// Loop for creating slides
+for (var i = 0; i < slideData.length; i++) {
+  console.log(slideData);
+  slideCon += Mustache.render(slideList, slideData[i]);
+}
+//Display slides in html
+elem.insertAdjacentHTML('beforeend', slideCon);
+
 var flkty = new Flickity( elem, {
   // options
   hash: true,
@@ -25,9 +36,3 @@ flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
 });
-
-
-
-
-
-
