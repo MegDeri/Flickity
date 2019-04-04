39de74b8a1eefc3c9a7b032ea0f21f938010a62a
Magda Aderibigbe <magdaaderibigbe@gmail.com>
2019-04-04 23:42:43 +0300

    Final changes js

--- js/script.js
+++ js/script.js
@@ -16,23 +16,3 @@ var flkty = new Flickity( elem, {
   cellAlign: 'left',
   contain: true,
 });
-
-var buttonGroup = document.querySelector('.button-group');
-var buttons = buttonGroup.querySelectorAll('.button');
-buttons = fizzyUIUtils.makeArray( buttons );
-
-buttonGroup.addEventListener( 'click', function( event ) {
-  // filter for button clicks
-  if ( !matchesSelector( event.target, '.button' ) ) {
-    return;
-  }
-  var index = buttons.indexOf( event.target );
-  flkty.select( index );
-});
-
-var progressBar = document.querySelector('.progress-bar')
-
-flkty.on( 'scroll', function( progress ) {
-  progress = Math.max( 0, Math.min( 1, progress ) );
-  progressBar.style.width = progress * 100 + '%';
-});
